7bc1d0eea7a0059f8c00645fd8e69e6faf595904
jose <jcmc@fe.up.pt>
2024-08-22 12:55:39 +0100

    Added missing comment.

--- data/benchmarks-metadata/Bugs.jar/get_trigger_tests.pl
+++ data/benchmarks-metadata/Bugs.jar/get_trigger_tests.pl
@@ -109,6 +109,7 @@ for my $failing_test_class (@{$failing_test_classes}) {
   }
   for my $line(@test_log_lines) {
     if ($line =~ /^Failed tests:\s*(.*)\[([^\]]*)\]\($failing_test_class\)/) {
+      # Parameterized test method
       push @trigger_tests, "${failing_test_class}::$1";
     } elsif ($line =~ /^Failed tests:\s*(.*)\($failing_test_class\)/) {
       # No-parameterized test method
